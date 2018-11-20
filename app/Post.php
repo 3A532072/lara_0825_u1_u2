a3ec5c9cadf2e45af29e4fe92f42e992efef55c9
3A532072 <dinel1138@gmail.com>
2018-11-21 07:48:56 +0800

    7-2:use Comment

--- app/Post.php
+++ app/Post.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Comment;
 
 class Post extends Model
 {
@@ -12,4 +13,7 @@ class Post extends Model
         'content',
         'is_feature'
     ];
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
 }
