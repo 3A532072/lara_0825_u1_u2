62b2bf682422b84fc8b3595e52df43626a8ded66
3A532072 <dinel1138@gmail.com>
2018-11-21 04:55:49 +0800

    修正

--- database/migrations/2018_11_13_233504_create_posts_table.php
+++ database/migrations/2018_11_13_233504_create_posts_table.php
@@ -2,7 +2,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
- class AddIsFeatureInPostsTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -11,22 +11,20 @@ use Illuminate\Database\Migrations\Migration;
      */
     public function up()
     {
-        Schema::table('posts', function (Blueprint $table) {
-            //
-            $table->string('title', 500)->change();
-            $table->boolean('is_feature')->default(false)->after('content');
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('content');
+            $table->timestamps();
         });
     }
-     /**
+    /**
      * Reverse the migrations.
      *
      * @return void
      */
     public function down()
     {
-        Schema::table('posts', function (Blueprint $table) {
-            $table ->string('title') ->change();
-            $table ->dropColumn('is_feature') ;
-        });
+        Schema::dropIfExists('posts');
     }
 }
