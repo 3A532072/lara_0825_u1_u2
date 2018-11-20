84f31cadd2b000e609f6a826ece72ae506f22553
3A532072 <dinel1138@gmail.com>
2018-11-21 07:29:11 +0800

    更新資料: save

--- routes/web.php
+++ routes/web.php
@@ -30,11 +30,14 @@ Route::get('/', function () {
     $posts = \App\Post::where('id', '<', 10)->orderBy('id', 'DESC')->get();*/
     $post = \App\Post::find(1);
 
-    //更新資料
+    /*更新資料
     $post->update([
         'title' => 'updated title',
         'content' => 'updated content',
-    ]);
+    ]);*/
+    $post->title = 'saved title';
+    $post->content = 'saved content';
+    $post->save();
     //dd($posts);
 
 
